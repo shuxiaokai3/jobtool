0332369df87e904e1082392b6d167cc2bf9a4b59
shuzhikai <2581105856@qq.com>
2020-06-26 22:40:01 +0800

    feat: 新增新建文件夹弹窗

---
+++ src/pages/modules/apidoc/doc-edit/dialog/add-folder.vue
@@ -0,0 +1,84 @@
+/*
+    创建者：shuxiaokai
+    创建时间：2020-06-26 19:07
+    模块名称：新增文件夹弹窗
+    备注：xxxx
+*/
+<template>
+    <s-dialog title="新建文件夹" :isShow="visible" width="40%" @close="handleClose">
+        <s-form ref="form" :formInfo="formInfo">
+            <s-form-item label="文件夹名称" vModel="name" oneLine required></s-form-item>
+        </s-form>  
+        <div slot="footer">
+            <el-button :loading="loading" size="mini" type="primary" @click="handleAddFolder">确定</el-button>
+            <el-button size="mini" type="warning" @click="handleClose">取消</el-button>
+        </div>
+    </s-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        visible: { //是否现实弹窗
+            type: Boolean,
+            default: false
+        },
+        pid: { //文档父id
+            type: String,
+            default: ""
+        },
+    },
+    data() {
+        return {
+            formInfo: {}, //------新增文档参数
+            //=====================================其他参数====================================//
+            loading: false, //----确认按钮状态
+        };
+    },
+    created() {
+
+    },
+    methods: {
+        //=====================================获取远程数据==================================//
+
+        //=====================================前后端交互====================================//
+
+        //=====================================组件间交互====================================//  
+        handleAddFolder() {
+            this.$refs["form"].validate((valid) => {
+                if (valid) {
+                    this.loading = true;
+                    const params = {
+                        docName: this.formInfo.name,
+                        isFolder: true,
+                        projectId: this.$route.query.id,
+                        pid: "",
+                    };
+                    this.axios.post("/api/project/new_doc", params).then((res) => {
+                        this.$emit("success", res.data);
+                        this.handleClose();
+                    }).catch(err => {
+                        this.$errorThrow(err, this);
+                    }).finally(() => {
+                        this.loading = false;
+                    });
+                } else {
+                    this.$message.warning("请完善必填信息");
+                    this.loading = false;
+                }
+            });
+        },
+        //=====================================其他操作=====================================//
+        //关闭弹窗
+        handleClose() {
+            this.$emit("update:visible", false);
+        },
+    }
+};
+</script>
+
+
+
+<style lang="scss">
+
+</style>
