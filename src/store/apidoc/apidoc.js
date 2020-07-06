19dfd58a020026174a1b3f84c10f0e8a76aacacf
舒志凯 <2581105856@qq.com>
2020-07-06 17:43:51 +0800

    feat: 更新当前选中的tab信息

--- src/store/apidoc/apidoc.js
+++ src/store/apidoc/apidoc.js
@@ -35,6 +35,15 @@ export default {
                 localStorage.setItem("apidoc/editTabs", JSON.stringify(state.tabs))
             }
         },
+        //改变某个tab信息
+        changeTabInfoById(state, payload) {
+            const { _id, projectId, docName } = payload;
+            const matchedData = state.tabs[projectId]?.find(val => val._id === _id);
+            if (matchedData && docName) {
+                matchedData.docName = docName;
+            }
+            localStorage.setItem("apidoc/editTabs", JSON.stringify(state.tabs))
+        },
         //更新所有tab
         updateAllTabs(state, payload) {
             const { projectId, tabs } = payload;
@@ -44,18 +53,7 @@ export default {
             state.tabs[projectId] = tabs;
             localStorage.setItem("apidoc/editTabs", JSON.stringify(state.tabs))
         },
-
-        //更新当前被选中的文档(不能未folder)
-        changeCurrentTab(state, payload) {
-            const { projectId, activeNode } = payload;
-            const isInProject = state.activeDoc[projectId]; //当前项目是否存在tabs
-            if (!isInProject) {
-                Vue.set(state.activeDoc, projectId, {});
-            }
-            state.activeDoc[projectId] = activeNode;
-            localStorage.setItem("apidoc/activeTab", JSON.stringify(state.activeDoc))
-        },
-        //根据位置删除
+        //根据位置删除tab
         deleteTabByPosition(state, payload) {
             const { projectId, start, num } = payload;
             if (state.tabs[projectId]) {
@@ -63,7 +61,7 @@ export default {
             }
             localStorage.setItem("apidoc/editTabs", JSON.stringify(state.tabs))
         },
-        //根据id删除
+        //根据id删除tab
         deleteTabById(state, payload) {
             const { projectId, deleteIds } = payload;
             if (state.tabs[projectId]) {
@@ -79,61 +77,26 @@ export default {
                 
             }
         },
-
-        //改变当前file信息
-        changeCurrentFileTab(state, payload) {
-            const { projectId, id, title, method, nodeType } = payload;
-            if (!id) { //清空fileTab
-                Vue.set(state.currentFileTab, payload.projectId, null);
-                localStorage.setItem("currentFileTab", JSON.stringify(state.currentTab));
-                return;
+        //=====================================当前选中的tab====================================//
+        //更新当前被选中的文档(不能未folder)
+        changeCurrentTab(state, payload) {
+            const { projectId, activeNode } = payload;
+            const isInProject = state.activeDoc[projectId]; //当前项目是否存在tabs
+            if (!isInProject) {
+                Vue.set(state.activeDoc, projectId, {});
             }
-            const docInfo = { id, title, method, projectId, nodeType };
-            Vue.set(state.currentFileTab, projectId, docInfo);
-            localStorage.setItem("currentFileTab", JSON.stringify(state.currentFileTab))
+            state.activeDoc[projectId] = activeNode;
+            localStorage.setItem("apidoc/activeTab", JSON.stringify(state.activeDoc))
         },
-        //改变tabs文档信息
-        changeTabInfo(state, payload) {
-            const { tabId, projectId, method } = payload;
-            if (state.tabs && state.tabs[projectId]) {
-                state.tabs[projectId].forEach(tab => {
-                    if (tab.id === tabId) {
-                        tab.method = method;
-                    }
-                })
+        //改变当前选中tab的基本信息
+        changeCurrentTabById(state, payload) {
+            const { projectId, docName } = payload;
+            const matchedData = state.activeDoc[projectId];
+            if (matchedData && docName) {
+                matchedData.docName = docName;
             }
-            localStorage.setItem("currentFileTab", JSON.stringify(state.currentFileTab))
+            localStorage.setItem("apidoc/activeTab", JSON.stringify(state.activeDoc))
         },
-        //删除一个tab
-        deleteTab(state, payload) {
-            const { projectId, id } = payload;
-            let deleteIndex = 0;
-            const allTabs = state.tabs[projectId]
-            if (allTabs) {
-                deleteIndex = allTabs.findIndex(val => val.id === id)
-                if (state.currentTab[projectId] && id === state.currentTab[projectId].id) {
-                    if (allTabs[deleteIndex + 1] != null) { //右侧还有标签
-                        this.commit("changeCurrentTab", {
-                            projectId,
-                            ...allTabs[deleteIndex + 1]
-                        })
-                    } else if (allTabs[deleteIndex + 1] == null && allTabs[deleteIndex - 1] != null) { //右侧没有标签并且左侧有标签
-                        this.commit("changeCurrentTab", {
-                            projectId,
-                            ...allTabs[deleteIndex - 1]
-                        })
-                    } else if (allTabs[deleteIndex + 1] == null && allTabs[deleteIndex - 1] == null) { //左右都不存在id
-                        this.commit("changeCurrentTab", { projectId });
-                        this.commit("changeCurrentFileTab", { projectId });
-                    }
-                    localStorage.setItem("currentTab", JSON.stringify(state.currentTab));
-                }
-                allTabs.splice(deleteIndex, 1);
-                localStorage.setItem("editDocTabs", JSON.stringify(state.tabs))
-                // localStorage.setItem("tabs", JSON.stringify(state.tabs[projectId]));   
-            }
-        },
-
     },
     actions: {
         async getDocBanner(context, payload) {
