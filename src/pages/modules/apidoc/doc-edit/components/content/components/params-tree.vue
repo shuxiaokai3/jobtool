edc69c672c8d0827f243a3e5a97b73c0cb4c2da4
舒志凯 <2581105856@qq.com>
2020-07-07 18:01:59 +0800

    feat: 新增文档编辑页面

---
+++ src/pages/modules/apidoc/doc-edit/components/content/components/params-tree.vue
@@ -0,0 +1,106 @@
+/*
+    创建者：shuxiaokai
+    创建时间：2020-07-07 15:56
+    模块名称：
+    备注：xxxx
+*/
+<template>
+    <el-collapse v-model="activeCollapse" class="collapse-wrap">
+        <el-collapse-item name="1">
+            <template slot="title">
+                请求头
+            </template>
+            <div class="params-edit">
+                <el-tree 
+                        :data="paramsTreeData" 
+                        :indent="30"
+                        :highlight-current="true"
+                        node-key="_id" 
+                        :expand-on-click-node="false" 
+                        default-expand-all
+                >
+                    <template slot-scope="scope">
+                        <div class="custom-tree-node">
+                            <el-button type="text" title="添加嵌套数据" icon="el-icon-plus" @click="addNestTreeData"></el-button>
+                            <el-button class="mr-2" title="删除当前项" type="text" icon="el-icon-close" @click="deleteTreeData(scope)"></el-button>
+                            <el-input v-model="scope.data.key" size="mini" placeholder="参数名称，例如：age name job" class="w-20 mr-2" @input="addNewLine"></el-input>
+                            <el-select v-model="scope.data.type" placeholder="类型" size="mini" class="w-10 mr-2">
+                                <el-option label="String" value="string"></el-option>
+                                <el-option label="Number" value="number"></el-option>
+                                <el-option label="Boolean" value="boolean"></el-option>
+                                <el-option label="Object" value="object"></el-option>
+                                <el-option label="List | Array" value="array"></el-option>
+                            </el-select>
+                            <el-select v-if="scope.data.type === 'boolean'" v-model="scope.data.value" placeholder="请选择" size="mini" class="w-25 mr-2">
+                                <el-option label="true" value="true"></el-option>
+                                <el-option label="false" value="false"></el-option>
+                            </el-select>
+                            <el-input v-else v-model="scope.data.value" size="mini" placeholder="参数值,例如 20 张三" maxlength="255" class="w-25 mr-2"></el-input>
+                            <!-- <el-checkbox v-model="scope.data.required" label={this.treeType === "request" ? "必填" : "必有"}></el-checkbox> -->
+                            <el-input v-model="scope.data.description" size="mini" placeholder="描述" maxlength="100" class="w-40"></el-input>
+                        </div>
+                    </template>
+                </el-tree>
+            </div>             
+        </el-collapse-item>
+    </el-collapse>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeCollapse: "1",
+            paramsTreeData: [{
+                x: 1
+            }],
+        };
+    },
+    created() {
+
+    },
+    methods: {
+        //添加嵌套数据
+        addNestTreeData() {
+
+        },
+        //删除数据
+        deleteTreeData() {
+
+        },
+        //新增一行
+        addNewLine() {
+
+        },
+        //=====================================组件间交互====================================//  
+        
+        //=====================================其他操作=====================================//
+
+    }
+};
+</script>
+
+
+
+<style lang="scss">
+.collapse-wrap {
+    .el-collapse-item__header {
+        background: $gray-200;
+    }
+    .params-edit {
+        .el-tree-node__content {
+            height: 50px;
+        }
+        .el-input__inner {
+            border-radius: 0;
+            border: none;
+            border-bottom: 1px solid $gray-300;
+        }
+        .custom-tree-node {
+            width: 100%;
+            display: flex;
+            align-items: center;
+        }
+    }
+}
+</style>
--- src/pages/modules/apidoc/doc-edit/components/content/content.vue
+++ src/pages/modules/apidoc/doc-edit/components/content/content.vue
@@ -5,16 +5,86 @@
     备注：xxxx
 */
 <template>
-    <div>
-        sadsad 
+    <div class="content">
+        <!-- 请求区域 -->
+        <div class="request">
+            <div class="config w-50">
+                <div class="d-flex mb-2">
+                    <el-input v-model="request.url.path" placeholder="请输入内容" size="small" @blur="checkUrlRule">
+                        <div slot="prepend" class="request-input">
+                            <el-select v-model="request.methods">
+                                <el-option value="get" label="get"></el-option>
+                                <el-option value="post" label="post"></el-option>
+                                <el-option value="put" label="put"></el-option>
+                                <el-option value="delete" label="delete"></el-option>
+                            </el-select>
+                        </div>
+                    </el-input> 
+                    <el-button type="success" size="small">发送请求</el-button>
+                </div>
+                <div class="d-flex a-center">
+                    <span class="flex0">接口描述：</span>
+                    <el-input v-model="request.description" size="small" placeholder="请输入接口描述" class="w-100" maxlength="255" clearable></el-input>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+            <div class="result">
+            </div>
+        </div>
+        <s-params-tree></s-params-tree>
+
     </div>
 </template>
 
 <script>
+import paramsTree from "./components/params-tree"
 export default {
+    components: {
+        "s-params-tree": paramsTree
+    },
     data() {
         return {
-
+            //=====================================请求基本信息====================================//
+            request: {
+                methods: "get", //---------------请求方式
+                url: {
+                    host: "", //-----------------主机(服务器)地址
+                    path: "", //-----------------接口路径
+                }, //----------------------------请求地址信息
+                requestParams: [
+                    {
+                        key: "", //--------------请求参数键
+                        value: "", //------------请求参数值
+                        type: "", //-------------请求参数值类型
+                        description: "", //------描述
+                        required: true, //-------是否必填
+                        children: [], //---------子参数
+                    }
+                ],
+                responseParams: [
+                    {
+                        key: "", //--------------请求参数键
+                        value: "", //------------请求参数值
+                        type: "", //-------------请求参数值类型
+                        description: "", //------描述
+                        required: true, //-------是否必填
+                        children: [], //---------子参数
+                    }
+                ],
+                header: [
+                    {
+                        key: "", //--------------请求头键
+                        value: "", //------------请求头值
+                        type: "string", //-------请求头值类型
+                        description: "", //------描述
+                        required: true, //-------是否必填
+                        children: [], //---------子参数
+                    }
+                ], //----------------------------请求头信息
+                description: "", //--------------请求描述
+            },
         };
     },
     created() {
@@ -26,7 +96,22 @@ export default {
         //=====================================前后端交互====================================//
 
         //=====================================组件间交互====================================//  
-        
+        //验证请求url格式是否正确
+        checkUrlRule() {
+            const hostHasSlash = this.request.url.host.endsWith("/");
+            const pathHasSlash = this.request.url.path.startsWith("/");
+            this.convertQueryToParams();
+
+            if (hostHasSlash && !pathHasSlash) {
+                return
+            } else if (!hostHasSlash && pathHasSlash) {
+                return
+            } else if (!hostHasSlash && !pathHasSlash) {
+                this.request.url.path = "/" + this.request.url.path;
+            } else if (hostHasSlash && pathHasSlash) {
+                this.request.url.path = this.request.url.path.slice(1);
+            }
+        },
         //=====================================其他操作=====================================//
 
     }
@@ -36,5 +121,20 @@ export default {
 
 
 <style lang="scss">
-
+.content {
+    padding: size(20);
+    .request {
+        display: flex;
+        width: 80%;
+        .request-input {
+            display: flex;
+            align-items: center;
+            height: 100%;
+            .el-select {
+                width: 100px;
+            }
+        }
+        .config {}
+    }
+}
 </style>
