007bc7f0f245acea9dc7c85cfc3dc1a626feb468
shuzhikai <2581105856@qq.com>
2020-07-12 21:24:56 +0800

    feat: 更新数组类型显示

--- src/pages/modules/apidoc/doc-edit/components/content/components/response.vue
+++ src/pages/modules/apidoc/doc-edit/components/content/components/response.vue
@@ -71,51 +71,49 @@ export default {
         convertPlainParamsToTreeData(plainData) {
             // console.log(plainData)
             const result = {};
-            const foo = (plainData, result) => {
+            const result2 = "";
+            const foo = (plainData, result, result2) => {
                 for(let i = 0,len = plainData.length; i < len; i++) {
-                    console.log(plainData[i])
                     const key = plainData[i].key.trim();
                     const value = plainData[i].value;
                     const type = plainData[i].type;
+                    const resultIsArray = Array.isArray(result);
                     const desc = plainData[i].description;
                     const isComplex = (type === "object" || type === "array");
+                    let arrTypeResultLength = 0; //数组类型值长度，用于数组里面嵌套对象时候对象取值
                     if (isComplex && key === "") { //复杂数据必须填写参数名称
                         continue;
                     }
-                    if (!isComplex && (key === "" || value === "")) { //非复杂数据需要填写参数名称
+                    if (!isComplex && (key === "" || value === "")) { //非复杂数据需要填写参数名称才可以显示
                         continue
                     }
-                    // console.log(type)    
                     /*eslint-disable indent*/ 
                     switch (type) {
                         case "number": //数字类型需要转换为数字，转换前所有值都为字符串
-                            result[key] = Number(value);
+                            resultIsArray ? result.push(Number(value)) : result[key] = Number(value);
                             break;
                         case "boolean": //字符串类型不做处理
-                            result[key] = (value === "true" ? true : false);
+                            resultIsArray ? result.push(result[key] = (value === "true" ? true : false)) : (result[key] = (value === "true" ? true : false));
                             break;
                         case "object":
-                            console.log(plainData[i].children, 999)
-                            result[key] = {};
-                            if (plainData[i].children && plainData[i].children.lengh > 0) {
-                                foo(plainData[i].children, result[key]);
+                            resultIsArray ? (arrTypeResultLength = result.push({})) : result[key] = {};
+                            if (plainData[i].children && plainData[i].children.length > 0) {
+                                foo(plainData[i].children, result[arrTypeResultLength - 1], result2);
                             }
                             break;
                         case "array":
-                            result[key] = {};
-                            if (plainData[i].children && plainData[i].children.lengh > 0) {
-                                foo(plainData[i].children, result[key]);
+                            result[key] = [];
+                            if (plainData[i].children && plainData[i].children.length > 0) {
+                                foo(plainData[i].children, result[key], result2);
                             }
                             break;
                         default: //字符串或其他类型类型不做处理
-                            result[key] = value;
+                            resultIsArray ? result.push(value) : (result[key] = value);
                             break;
                     }
-
-
                 }
             }
-            foo(plainData, result);
+            foo(plainData, result, result2);
             return result;
         },
         //=====================================其他操作=====================================//
