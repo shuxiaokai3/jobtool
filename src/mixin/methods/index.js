3680d027eb840073b66528adcd14443513288767
shuxiaokai <2581105856@qq.com>
2020-08-05 15:44:49 +0800

    refactor: 重构全局方法

--- src/mixin/methods/index.js
+++ src/mixin/methods/index.js
@@ -1,72 +1,8 @@
 import tips from "./tips"
-import api from "@/api/api.js"
-const axios = api.axios;
-
 export default {
-
-    //=====================================节流====================================//
-    sDebounce(fn, delay = 1000) {
-        return function() {
-            setTimeout(() => {
-                fn.apply(this, arguments);
-            }, delay);
-        };
-    },
-    //=====================================深拷贝====================================//
-    deepCopy(obj) {
-        let clone = {};
-        if (
-            obj == null ||
-            Array.prototype.toString.call(obj).slice(8, -1) === "Date" ||
-            typeof obj !== "object"
-        ) {
-            return obj;
-        }
-        if (Array.isArray(obj)) {
-            clone = [];
-            obj.forEach((val, index) => {
-                clone[index] = this.deepCopy(val);
-            });
-            return clone;
-        }
-        for (const i in obj) {
-            if (obj[i] === null) {
-                clone[i] = obj[i];
-            } else if (Array.isArray(obj[i])) {
-                clone[i] = [];
-                obj[i].forEach((val, index) => {
-                    clone[i][index] = this.deepCopy(val);
-                });
-            } else if (typeof obj[i] === "object") {
-                clone[i] = this.deepCopy(obj[i]);
-            } else {
-                clone[i] = obj[i];
-            }
-        }
-        return clone;
-    },
-    globalDownload(url) {
-        axios.get(url, {
-            responseType: "blob",
-        }).then(res => {
-            let blobUrl = "";
-            blobUrl = URL.createObjectURL(res.blob);
-            const downloadElement = document.createElement("a");
-            downloadElement.href = blobUrl;
-            downloadElement.download = decodeURIComponent(res.fileName) || "未命名"; //下载后文件名
-            document.body.appendChild(downloadElement);
-            downloadElement.click(); //点击下载
-            document.body.removeChild(downloadElement); //下载完成移除元素
-            window.URL.revokeObjectURL(blobUrl); //释放掉blob对象
-        }).catch(err => {
-            this.$errorThrow(err, this);
-        }).finally(() => {
-            this.loading = false;
-        });   
-    },
-    /* 
+    /**
         @description  随机生成提示信息
-        @author        shuxiaokai
+        @autor        shuxiaokai
         @create       2019-06-25 13:33"
         @params       
         @return       
@@ -81,37 +17,75 @@ export default {
             return;
         }
     },
-    /* 
-        @description  删除字段值为 "", null, undefined的字段 
-        @author        shuxiaokai
-        @create       2019-07-02 17:03"
-        @params       data(原始值)
-        @params       deep(是否遍历子对象); 只针对
-        @return       data(最新值)
-    */
-    deleteEmptyPath(data, deep = false) {
-        if (data == null || Object.prototype.toString.call(data) !== "[object Object]") {
-            throw new Error("传入参数只能为对象类型");
+
+    /** 
+     * @description        获取祖先组件
+     * @autor              shuxiaokai
+     * @updateAuthor       shuxiaokai
+     * @create             2020-02-13 15:22
+     * @update             2020-02-13 15:22
+     * @param {String}     componentName - 组件名称       
+     * @return {any}       若查询到组件则返回组件，否则返回null
+     */
+    getAncestorComponent(componentName = "") {
+        let parent = this.$parent || null;
+        while (parent && parent.$options.name !== componentName) {
+            parent = parent.$parent || null;
         }
-        if (!deep) {
-            for (const i in data) {
-                const hasOwn = Object.hasOwnProperty;
-                if (hasOwn.call(data, i) && (data[i] == null || data[i] === "")) {
-                    delete data[i];
+        return parent;
+    },
+
+    /** 
+     * @description        向父元素触发事件
+     * @autor              shuxiaokai
+     * @updateAuthor       shuxiaokai
+     * @param {String}     componentName - 组件名称       
+     * @param {String}     eventName - 事件名称       
+     * @param {String}     params - 参数      
+     * @create             2020-02-10 11:21
+     * @update             2020-02-10 11:21
+     */
+
+    dispatch(componentName, eventName, params) {
+        let parent = this.$parent;
+        if (parent.$options.name === componentName) {
+            // console.log(parent)
+            parent.$emit(eventName, params);
+            return;
+        }
+        while (parent) {
+            if (parent.$options.name !== componentName) {
+                parent = parent.$parent;
+            } else {
+                // console.log(parent)
+                parent.$emit(eventName, params);
+                return;
+            }
+        }
+    },
+
+    /** 
+     * @description        向子元素广播事件
+     * @autor              shuxiaokai
+     * @updateAuthor       shuxiaokai
+     * @param {String}     componentName - 组件名称       
+     * @param {String}     eventName - 事件名称       
+     * @param {String}     params - 参数      
+     * @create             2020-02-10 11:21
+     * @update             2020-02-10 11:21
+     */
+    broadcast(componentName, eventName, params) {
+        const foo = (data, componentName, eventName, params) => {
+            for(let i = 0; i < data.length; i++) {
+                if (data[i].$options.name === componentName) {
+                    data[i].$emit(eventName, params);
+                }
+                if (data[i].$children && data[i].$children.length > 0) {
+                    foo(data[i].$children, componentName, eventName, params);
                 }
             }
-        } else {
-            // const _foo = function foo(val) {
-            //     for (const i in val) {
-            //         if (val.hasOwnProperty(i) && Object.prototype.toString.call(val) !== "[object Object]") {
-            //             a(val[i]);
-            //         }
-            //         if (val.hasOwnProperty(i) && (val[i] == null || val[i] === "")) {
-            //             delete val[i];
-            //         }
-            //     }
-            // }
         }
+        foo(this.$children, componentName, eventName, params);
     }
 };
 
